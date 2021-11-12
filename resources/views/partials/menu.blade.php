4f3e8715932082c33e7d54bcf98b36787212a89d
mujeeb <mujeeb>
2021-11-12 06:31:33 +0000

    Edited menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -99,7 +99,7 @@
         @endcan
         @can('generate_sop_access')
             <li class="c-sidebar-nav-item">
-                <a href="{{ route("admin.generate-sops.index") }}" class="c-sidebar-nav-link {{ request()->is("admin/generate-sops") || request()->is("admin/generate-sops/*") ? "c-active" : "" }}">
+                <a href="{{ route("admin.generatesop.create") }}" class="c-sidebar-nav-link {{ request()->is("admin/generate-sops") || request()->is("admin/generate-sops/*") ? "c-active" : "" }}">
                     <i class="fa-fw fas fa-cogs c-sidebar-nav-icon">
 
                     </i>
