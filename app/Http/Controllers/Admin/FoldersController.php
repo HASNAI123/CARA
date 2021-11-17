ceda6b37bd26042a684ac187c078b733704ffeac
mujeeb <mujeeb>
2021-11-17 19:23:43 +0000

    Edited FoldersController.php

--- app/Http/Controllers/Admin/FoldersController.php
+++ app/Http/Controllers/Admin/FoldersController.php
@@ -161,7 +161,7 @@ class FoldersController extends Controller
          foreach($ids as $id){
             $id->password;
          }
-          if(empty($id->password)){
+          if($id->password==""){
              return "empty";
           }else{
 
