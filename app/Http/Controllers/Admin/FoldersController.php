509bff3b26d25d288f360843bd283e74d380bcc3
mujeeb <mujeeb>
2021-11-17 19:19:43 +0000

    Edited FoldersController.php

--- app/Http/Controllers/Admin/FoldersController.php
+++ app/Http/Controllers/Admin/FoldersController.php
@@ -159,7 +159,7 @@ class FoldersController extends Controller
     {   
           $ids=DB::table('folders')->where('id',$id)->get();
          foreach($ids as $id){
-            return $id->password;
+            $id->password;
          }
           if(empty($id->password)){
              return "empty";
