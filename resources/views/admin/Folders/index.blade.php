1f9e51fdddbec1a0283cdd3c84c35f776ba2c9d7
mujeeb <mujeeb>
2021-11-21 08:35:11 +0000

    Edited index.blade.php

--- resources/views/admin/Folders/index.blade.php
+++ resources/views/admin/Folders/index.blade.php
@@ -149,7 +149,7 @@
                                     @endcan
                                         
                                     @can('Folder_delete')    
-                                     <form action="{{ route('admin.folders.destroy', $folders->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                     <form action="{{ route('admin.folders.destroy', $folders->title) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                         <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
