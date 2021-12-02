21b5c35110c6410c682d7cb19b6939db90bb504f
mujeeb <mujeeb>
2021-12-02 17:41:59 +0000

    Edited edit.blade.php

--- resources/views/admin/Folders/edit.blade.php
+++ resources/views/admin/Folders/edit.blade.php
@@ -41,10 +41,10 @@
                
                 
                 <br><br>
-                @can('Password_edit')
+                
                 <label for="Business unit">{{ trans('Update Password') }}</label>
-                <input class="form-control {{ $errors->has('business_unit') ? 'is-invalid' : '' }}" type="password" name="password" id="business_unit" value="">
-                @endcan       
+                <input class="form-control {{ $errors->has('business_unit') ? 'is-invalid' : '' }}" type="password" name="password" id="business_unit" value="{{$folder->password}}">
+                    
              
                 <span class="help-block">{{ trans('cruds.sop.fields.uploaded_by_helper') }}</span>
             </div>
