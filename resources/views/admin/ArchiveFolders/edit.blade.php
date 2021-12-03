c0186fc60a9504c81c2f08cee227b6ac0a603fa4
nuraini <nurainiselamat@gmail.com>
2021-12-03 08:02:08 +0000

    Edited edit.blade.php

--- resources/views/admin/ArchiveFolders/edit.blade.php
+++ resources/views/admin/ArchiveFolders/edit.blade.php
@@ -45,7 +45,7 @@
                  
                  <script>
                     function myFunction() {
-                      var x = document.getElementByTagName("password");
+                      var x = document.getElementByName("password");
                       if (x.type === "password") {
                         x.type = "text";
                       } else {
