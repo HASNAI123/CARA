e24c58565cbef0e19b51b0df6f85ef412f2d0947
hasnain <hasnainrashid1@live.com>
2021-12-08 17:43:22 +0000

    Edited admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -78,7 +78,7 @@
                         <div class="alert alert-danger">
                             <ul class="list-unstyled">
                                 @foreach($errors->all() as $error)
-                                    <li>{{ $error->message }}</li>
+                                    <li>{{ $error }}</li>
                                 @endforeach
                             </ul>
                         </div>
