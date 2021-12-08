58ba73d3f0bd1f9c0a61157046b2abd9b13f6546
hasnain <hasnainrashid1@live.com>
2021-12-08 17:20:38 +0000

    Edited admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -74,15 +74,7 @@
                             </div>
                         </div>
                     @endif
-                    @if($errors->count() > 0)
-                        <div class="alert alert-danger">
-                            <ul class="list-unstyled">
-                                @foreach($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+                    
                     @yield('content')
 
                 </div>
