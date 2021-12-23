9bf00952497cf7de1fae524bb14e7bd28776f520
hasnain <has@>
2021-12-23 20:09:40 +0000

    Edited pdf.blade.php

--- resources/views/admin/generatesop/pdf.blade.php
+++ resources/views/admin/generatesop/pdf.blade.php
@@ -309,7 +309,7 @@ foreach ($flow as $img) {
             $Steps= $a++.'-'.$new;
             @endphp
 
-            <td style="width: 30%; font-size:12px; vertical-align: top; "><b>{!! $Steps !!}</b></td>
+            <td style="width: 30%; font-size:12px; padding-top:12px; vertical-align: top; "><b>{!! $Steps !!}</b></td>
         
             @php
             $desc=$generatesop->desc[$key];
