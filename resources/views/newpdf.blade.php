2e07b82e48d60906ce0840f722d0d6a68110d0df
Mujeeb Rahman <mujeeb.rahujo@gmail.com>
2022-01-26 18:44:57 +0000

    Edited newpdf.blade.php

--- resources/views/newpdf.blade.php
+++ resources/views/newpdf.blade.php
@@ -355,7 +355,8 @@ $pdf->writeHTML('APPENDIX '. $b, true, false, false, false, '');
 
 if($generatesop->appendix){
 
-foreach ($images as $pkey=>$value) {    
+foreach ($images as $pkey=>$value) { 
+$d++;
 $appendix_images=file_get_contents(Storage::disk('s3')->url('images/'.$value));
 $pdf->Image('@' . $appendix_images, 0, 30, 180, 150, '', '', '', true, 200,'C');
 
