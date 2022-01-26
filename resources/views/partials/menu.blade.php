9fb1c26e65e9f529f9688e3d2ca8020ce4cef6d6
Nuraini <nurainiselamat@gmail.com>
2022-01-26 18:08:47 +0000

    Edited menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -66,7 +66,7 @@
                                 <i class="c-sidebar-nav-icon fas fa-fw fa-tachometer-alt">
 
                                 </i>
-                                {{ trans('Inprogress SOP') }}
+                                {{ trans('Draft SOP') }}
                             </a>
                         </li>
 
