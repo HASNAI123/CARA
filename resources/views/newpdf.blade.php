7c5a5c0d53d76f9274c66cdc522573da1311122c
Mujeeb <mujeeb.rahujo123@gmail.com>
2022-02-05 18:43:24 +0000

    Edited newpdf.blade.php

--- resources/views/newpdf.blade.php
+++ resources/views/newpdf.blade.php
@@ -359,13 +359,14 @@ if($generatesop->appendix){
 
 foreach ($images as $pkey=>$value) { 
 $d++
-if($value){
+if($generatesop->appendix){
 $appendix_images=file_get_contents(Storage::disk('s3')->url('images/'.$value))
-}
+
 $pdf->Image('@' . $appendix_images, 0, 30, 220, 150, '', '', '', true, 200,'C');
 
 if($d < $count){
 $pdf->AddPage('L');
+}
 }
 }
 }
