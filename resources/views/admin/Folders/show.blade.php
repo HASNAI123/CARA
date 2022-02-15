da773d20079999d1d790984f5ad460a1c21f6117
nurainiselamat <nurainiselamat@gmail.com>
2022-02-15 03:41:27 +0000

    Edited show.blade.php

--- resources/views/admin/Folders/show.blade.php
+++ resources/views/admin/Folders/show.blade.php
@@ -159,14 +159,18 @@
                                 <form method="POST" action="{{ route('admin.generatesop.show',$generatesop->id) }}" enctype="multipart/form-data">
                                         @method('GET')
                                         @csrf
-                                   <p><li> I hereby acknowledge that i am going to view and download the following documents from CARA (caramyaeon.com.my).<br>
-                                       <i>Saya dengan ini mengakui bahawa saya akan melihat dan memuat turun dokumen berikut daripada CARA (caramyaeon.com.my).</i></li></p>
+                                    
+                                  <ol type="1">
+                                  <li><p> I hereby acknowledge that i am going to view and download the following documents from CARA (caramyaeon.com.my).<br>
+                                       <i>Saya dengan ini mengakui bahawa saya akan melihat dan memuat turun dokumen berikut daripada CARA (caramyaeon.com.my).</i></p></li>
                                    
-                                   <p><li> I acknowledge the it is my responsbility to read, understand, and adhere to these procedures.<br>
-                                       <i>Saya mengakui tanggungjawab saya untuk membaca, memahami dan mematuhi prosedur ini.</i></li></p>
+                                   <li><p> I acknowledge the it is my responsbility to read, understand, and adhere to these procedures.<br>
+                                       <i>Saya mengakui tanggungjawab saya untuk membaca, memahami dan mematuhi prosedur ini.</i></p></li>
                                    
-                                   <p><li> I further understand that any failure to fully adhere to the said procedure by me may result in disciplinary action, including termination.<br>
-                                       <i>Saya selanjutnya memahami bahawa sebarang kegagalan untuk mematuhi sepenuhnya prosedur tersebut oleh saya boleh mengakibatkan tindakan tatatertib, termasuk pemberhentian.</i></li></p>
+                                   <li><p> I further understand that any failure to fully adhere to the said procedure by me may result in disciplinary action, including termination.<br>
+                                       <i>Saya selanjutnya memahami bahawa sebarang kegagalan untuk mematuhi sepenuhnya prosedur tersebut oleh saya boleh mengakibatkan tindakan tatatertib, termasuk pemberhentian.</i></p></li>
+                                  </ol>
+                                  
                                   
                                   <div class="row">
                                     <div class="col-md-6">
