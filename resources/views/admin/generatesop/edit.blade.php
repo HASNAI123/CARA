1479842585ed47741c09547b8ef11f5018ef0eef
mujeeb <mujeeb>
2022-02-28 07:48:45 +0000

    Edited edit.blade.php

--- resources/views/admin/generatesop/edit.blade.php
+++ resources/views/admin/generatesop/edit.blade.php
@@ -269,7 +269,7 @@ input[type=text]:focus {
         <label for="permissions">{{ trans('Assign To') }}
                                 <span class="btn btn-info btn-xs select-all">{{ trans('global.select_all') }}</span>
                                 <span class="btn btn-info btn-xs deselect-all">{{ trans('global.deselect_all') }}</span></label><br/>
-                            <select name="users[]" id="permissions" class="form-control select2" style="width:800px;"   multiple="multiple">
+                            <select name="users[]" id="permissions" class="form-control select2" style="width:800px;"   multiple="multiple" required="">
                               @foreach($users as $users)
                                     <option value="{{$users->id}}" {{in_array($users->id, explode(',',$generatesop->assign_to) ?: []) ? "selected" : ""}}>{{$users->name}}</option>
 
