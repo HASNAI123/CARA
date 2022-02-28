c8725d40b310ccb145bdaab26411a4a9c2009298
mujeeb <mujeeb>
2022-02-28 11:10:38 +0000

    Edited ContactController.php

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        return view('Admin.contact.index');
+        return view('admin.contact.index');
     }
 
     /**
