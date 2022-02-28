724c2114593fbe391cf40319330e4b77df32da74
mujeeb <mujeeb>
2022-02-28 07:32:54 +0000

    Edited edit.blade.php

--- resources/views/admin/generatesop/edit.blade.php
+++ resources/views/admin/generatesop/edit.blade.php
@@ -271,7 +271,7 @@ input[type=text]:focus {
                                 <span class="btn btn-info btn-xs deselect-all">{{ trans('global.deselect_all') }}</span></label><br/>
                             <select name="users[]" id="permissions" class="form-control select2" style="width:800px;"   multiple="multiple">
                               @foreach($users as $users)
-                                    <option value="{{$users->id}}">{{$users->name}}</option>
+                                    <option value="{{$users->id}}" {{in_array($users->id, explode(',',$generatesop->assign_to) ?: []) ? "selected" : ""}}>{{$users->name}}</option>
 
                               @endforeach      
                
